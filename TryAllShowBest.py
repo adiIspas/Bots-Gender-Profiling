249f25675e96e66622b0c4e1f29e937aec6e6608
teo2mirce <teo2mircea@gmail.com>
2019-03-02 18:35:40 +0200

    Add files via upload

---
+++ TryAllShowBest.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+models.append(('Knn2',     KNeighborsClassifier(3) ))
+models.append(('LSVM',     SVC(kernel="linear", C=0.025)  ))
+models.append(('RBF SVM',     SVC(gamma=2, C=1)  ))
+# models.append(('Gaussian Process',     GaussianProcessClassifier(1.0 * RBF(1.0)) ))
+models.append(('DT',     DecisionTreeClassifier(max_depth=5) ))
+models.append(('RF',     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1) ))
+models.append(('NN',     MLPClassifier(alpha=1) ))
+models.append(('AB',     AdaBoostClassifier() ))
+models.append(('NB',     GaussianNB() ))
+models.append(('QDA',     QuadraticDiscriminantAnalysis()  ))
+
+
+f = open("x_features.txt")
+Features=f.readline().split(',')
+Features.pop(0)#Primu e clasa
+data = np.loadtxt(f,delimiter=",")
+Y=data[:,0]
+X=data[:,1:]
+
+
+
+
+results = []
+names = []
+scoring = 'accuracy'
+for name, model in models:
+	kfold = model_selection.StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+	cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+# boxplot algorithm comparison
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
