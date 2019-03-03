9e7b693911e1309b0d68c220fe197aa08f4db874
Adrian Ispas <adrian.ispas@ab4.systems>
2019-03-03 13:16:09 +0200

    Add new features and make feature extraction more generic by language.

--- src/process_dataset.py
+++ src/process_dataset.py
@@ -3,18 +3,18 @@ import datetime
 import src.preparation.xml_reader as xr
 import src.preparation.text_reader as tr
 import src.processing.data_processor as dp
-import src.processing.fetures_extraction as fe
 
 from os import listdir
+from src.processing.fetures_extraction import Features
 
 dataset = '../data'
 raw = '/raw'
 processed = '/processed'
-# language = '/en'
-language = '/es'
+# language = 'en'
+language = 'es'
 
-dataset_raw_path = dataset + raw + language + "/"
-dataset_processed_path = dataset + processed + language + "/"
+dataset_raw_path = dataset + raw + '/' + language + '/'
+dataset_processed_path = dataset + processed + '/' + language + '/'
 
 file_extension = '.xml'
 
@@ -23,24 +23,29 @@ start_time = time.time()
 authors_tweets = listdir(dataset_raw_path)
 authors_tweets = [author_tweets for author_tweets in authors_tweets if author_tweets.endswith(file_extension)]
 
-file_authors_classes = dataset_raw_path + "truth.txt"
+file_authors_classes = dataset_raw_path + 'truth.txt'
 
 authors_classes_train = tr.get_authors_classes(file_authors_classes)
 
 data = []
-columns = ['words_per_tweet', 'characters_per_tweet', 'average_word_len', 'number_of_stop_words',
-           'number_of_tags', 'number_of_hash_tags', 'readability', 'author_class']
+columns = ['words_per_tweet', 'characters_per_tweet', 'average_word_len', 'number_of_stop_words', 'number_of_tags',
+           'number_of_hash_tags', 'readability', 'number_of_digits', 'number_of_secure_links',
+           'number_of_unsecured_links', 'number_of_percent', 'number_of_exclamation_marks', 'number_of_question_marks',
+           'number_of_commas', 'number_of_points', 'number_of_male_terms', 'number_of_female_terms', 'number_of_emoji',
+           'author_class']
+
+fe = Features(language)
 for author_tweets in authors_tweets:
     author_id = author_tweets.replace(file_extension, '')
     print('Processing ', author_id)
 
     tweets = xr.get_tweets(dataset_raw_path + author_tweets)
-    features = fe.get_features(tweets)
+    features = fe.extract(tweets)
 
     if authors_classes_train.get(author_id) is not None:
         features.append(authors_classes_train.get(author_id))
         data.append(features)
 
-dp.create_csv_dataset(data, columns, dataset_processed_path + 'data.csv')
+dp.create_csv_dataset(data, columns, dataset_processed_path + language + '_data.csv')
 
-print("--- Total time of execution:  %s ---" % (datetime.timedelta(seconds=time.time() - start_time)))
+print('--- Total time of execution:  %s ---' % (datetime.timedelta(seconds=time.time() - start_time)))
--- src/processing/fetures_extraction.py
+++ src/processing/fetures_extraction.py
@@ -1,83 +1,164 @@
 import re
 import pyphen
+import emoji
 
 from nltk.corpus import stopwords
-
-# stop_words = stopwords.words('english')
-# dic = pyphen.Pyphen(lang='en_UK')
-stop_words = stopwords.words('spanish')
-dic = pyphen.Pyphen(lang='es_ES')
+from nltk.stem.snowball import SnowballStemmer
 
 
-def get_features(tweets):
-    number_of_words = 0
-    number_of_characters = 0
-    average_word_len = 0
-    number_of_stop_words = 0
-    number_of_tags = 0
-    number_of_hash_tags = 0
-    number_of_syllables = 0
-    number_of_secure_links = 0
-    number_of_unsecure_links = 0
-    number_of_digits = 0
-    number_of_percent = 0
-    total_tweets = len(tweets)
+class Features(object):
 
-    for tweet in tweets:
-        number_of_words += number_of_words_per_tweet(tweet)
-        number_of_characters += number_of_characters_per_tweet(tweet)
-        average_word_len += average_word_len_per_tweet(tweet)
-        number_of_stop_words += number_of_stop_words_per_tweet(tweet)
-        number_of_tags += number_of_tags_per_tweet(tweet)
-        number_of_hash_tags += number_of_hash_tags_per_tweet(tweet)
-        number_of_syllables += number_of_syllables_per_tweet(tweet)
+    def __init__(self, language):
+        self.languages_name_dict = {'en': 'english', 'es': 'spanish'}
+        self.languages_iso_dict = {'en': 'en_UK', 'es': 'es_ES'}
 
-    average_number_of_syllables_per_word = number_of_syllables/number_of_words if number_of_words > 0 else 0
-    number_of_words = number_of_words/total_tweets
-    number_of_characters = number_of_characters/total_tweets
-    average_word_len = average_word_len/total_tweets
-    number_of_stop_words = number_of_stop_words/total_tweets
-    number_of_tags = number_of_tags/total_tweets
-    number_of_hash_tags = number_of_hash_tags/total_tweets
-    readability = readability_level(number_of_words, average_number_of_syllables_per_word)
+        self.stop_words = stopwords.words(self.languages_name_dict.get(language))
+        self.syllables_dic = pyphen.Pyphen(lang=self.languages_iso_dict.get(language))
+        self.stemmer = SnowballStemmer(self.languages_name_dict.get(language))
 
-    return [number_of_words, number_of_characters, average_word_len,
-            number_of_stop_words, number_of_tags, number_of_hash_tags, readability]
+    def extract(self, tweets):
+        number_of_words = 0
+        number_of_characters = 0
+        average_word_len = 0
+        number_of_stop_words = 0
+        number_of_tags = 0
+        number_of_hash_tags = 0
+        number_of_syllables = 0
+        number_of_secure_links = 0
+        number_of_unsecured_links = 0
+        number_of_digits = 0
+        number_of_percent = 0
+        number_of_exclamation_marks = 0
+        number_of_question_marks = 0
+        number_of_commas = 0
+        number_of_points = 0
+        number_of_male_terms = 0
+        number_of_female_terms = 0
+        number_of_emoji = 0
 
+        total_tweets = len(tweets)
 
-def number_of_words_per_tweet(tweet):
-    return len(re.findall(r'\w+', tweet))
+        for tweet in tweets:
+            number_of_words += self.number_of_words_per_tweet(tweet)
+            number_of_characters += self.number_of_characters_per_tweet(tweet)
+            average_word_len += self.average_word_len_per_tweet(tweet)
+            number_of_stop_words += self.number_of_stop_words_per_tweet(tweet)
+            number_of_tags += self.number_of_tags_per_tweet(tweet)
+            number_of_hash_tags += self.number_of_hash_tags_per_tweet(tweet)
+            number_of_syllables += self.number_of_syllables_per_tweet(tweet)
+            number_of_digits += self.number_of_digits_per_tweet(tweet)
+            number_of_secure_links += self.number_of_secure_links_per_tweet(tweet)
+            number_of_unsecured_links += self.number_of_unsecured_links_per_tweet(tweet)
+            number_of_percent += self.number_of_percent_per_tweet(tweet)
+            number_of_exclamation_marks += self.number_of_exclamation_marks_per_tweet(tweet)
+            number_of_question_marks += self.number_of_question_marks_per_tweet(tweet)
+            number_of_commas += self.number_of_commas_per_tweet(tweet)
+            number_of_points += self.number_of_points_per_tweet(tweet)
+            number_of_male_terms += self.number_of_male_terms_per_tweet(tweet)
+            number_of_female_terms += self.number_of_female_terms_per_tweet(tweet)
+            number_of_emoji += self.number_of_emoji_per_tweet(tweet)
 
+        average_number_of_syllables_per_word = number_of_syllables / number_of_words if number_of_words > 0 else 0
+        number_of_words = number_of_words / total_tweets
+        number_of_characters = number_of_characters / total_tweets
+        average_word_len = average_word_len / total_tweets
+        number_of_stop_words = number_of_stop_words / total_tweets
+        number_of_tags = number_of_tags / total_tweets
+        number_of_hash_tags = number_of_hash_tags / total_tweets
+        readability = self.readability_level(number_of_words, average_number_of_syllables_per_word)
+        number_of_digits = number_of_digits / total_tweets
+        number_of_secure_links = number_of_secure_links / total_tweets
+        number_of_unsecured_links = number_of_unsecured_links / total_tweets
+        number_of_percent = number_of_percent / total_tweets
+        number_of_exclamation_marks = number_of_exclamation_marks / total_tweets
+        number_of_question_marks = number_of_question_marks / total_tweets
+        number_of_commas = number_of_commas / total_tweets
+        number_of_points = number_of_points / total_tweets
+        number_of_male_terms = number_of_male_terms / total_tweets
+        number_of_female_terms = number_of_female_terms / total_tweets
+        number_of_emoji = number_of_emoji / total_tweets
 
-def number_of_characters_per_tweet(tweet):
-    return len(tweet)
+        return [number_of_words, number_of_characters, average_word_len, number_of_stop_words, number_of_tags,
+                number_of_hash_tags, readability, number_of_digits, number_of_secure_links, number_of_unsecured_links,
+                number_of_percent, number_of_exclamation_marks, number_of_question_marks, number_of_commas,
+                number_of_points, number_of_male_terms, number_of_female_terms, number_of_emoji]
 
+    def number_of_syllables_per_tweet(self, tweet):
+        number_of_syllables = 0
 
-def average_word_len_per_tweet(tweet):
-    words = re.findall(r'\w+', tweet)
-    return sum(len(word) for word in words)/len(words) if len(words) > 0 else 0
+        for word in re.findall(r'\w+', tweet):
+            number_of_syllables += self.syllables_dic.inserted(word).count('-')
 
+        return number_of_syllables
 
-def number_of_stop_words_per_tweet(tweet):
-    return len([word for word in re.findall(r'\w+', tweet) if word in stop_words])
+    def number_of_male_terms_per_tweet(self, tweet):
+        tweet = ' '.join([self.stemmer.stem(word) for word in re.findall(r'\w+', tweet)])
+        return tweet.count('he') + tweet.count('his') + tweet.count('boy') + tweet.count('man')
 
+    def number_of_female_terms_per_tweet(self, tweet):
+        tweet = ' '.join([self.stemmer.stem(word) for word in re.findall(r'\w+', tweet)])
+        return tweet.count('she') + tweet.count('her') + tweet.count('girl') + tweet.count('woman')
 
-def number_of_tags_per_tweet(tweet):
-    return len([word for word in tweet.split() if str(word).startswith('@')])
+    @staticmethod
+    def number_of_words_per_tweet(tweet):
+        return len(re.findall(r'\w+', tweet))
 
+    @staticmethod
+    def number_of_characters_per_tweet(tweet):
+        return len(tweet)
 
-def number_of_hash_tags_per_tweet(tweet):
-    return len([word for word in tweet.split() if str(word).startswith('#')])
+    @staticmethod
+    def average_word_len_per_tweet(tweet):
+        words = re.findall(r'\w+', tweet)
+        return sum(len(word) for word in words) / len(words) if len(words) > 0 else 0
 
+    def number_of_stop_words_per_tweet(self, tweet):
+        return len([word for word in re.findall(r'\w+', tweet) if word in self.stop_words])
 
-def number_of_syllables_per_tweet(tweet):
-    number_of_syllables = 0
+    @staticmethod
+    def number_of_tags_per_tweet(tweet):
+        return len([word for word in tweet.split() if str(word).startswith('@')])
 
-    for word in re.findall(r'\w+', tweet):
-        number_of_syllables += dic.inserted(word).count('-')
+    @staticmethod
+    def number_of_hash_tags_per_tweet(tweet):
+        return len([word for word in tweet.split() if str(word).startswith('#')])
 
-    return number_of_syllables
+    @staticmethod
+    def readability_level(average_number_of_words, average_number_of_syllables_per_word):
+        return (0.39 * average_number_of_words) + (11.8 * average_number_of_syllables_per_word) - 15.59
 
+    @staticmethod
+    def number_of_digits_per_tweet(tweet):
+        return sum([sum(c.isdigit() for c in word) for word in tweet.split()])
 
-def readability_level(average_number_of_words, average_number_of_syllables_per_word):
-    return (0.39 * average_number_of_words) + (11.8 * average_number_of_syllables_per_word) - 15.59
+    @staticmethod
+    def number_of_secure_links_per_tweet(tweet):
+        return str(tweet).count('https')
+
+    @staticmethod
+    def number_of_unsecured_links_per_tweet(tweet):
+        return str(tweet).count('http')
+
+    @staticmethod
+    def number_of_percent_per_tweet(tweet):
+        return str(tweet).count('%')
+
+    @staticmethod
+    def number_of_exclamation_marks_per_tweet(tweet):
+        return str(tweet).count('!')
+
+    @staticmethod
+    def number_of_question_marks_per_tweet(tweet):
+        return str(tweet).count('?')
+
+    @staticmethod
+    def number_of_commas_per_tweet(tweet):
+        return str(tweet).count(',')
+
+    @staticmethod
+    def number_of_points_per_tweet(tweet):
+        return str(tweet).count('.')
+
+    @staticmethod
+    def number_of_emoji_per_tweet(tweet):
+        return emoji.demojize(tweet).count(':') / 2
